71c0220f69a59a1020b547647bf36b549e3f6850
httpsisbetter <119723641+httpsisbetter@users.noreply.github.com>
2024-06-26 19:31:49 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='nitrous-oxi',
+    name='nitrous-oxide',
     version='0.2.0',
-    description='A Python client for the Nitrous-Oxi API',
+    description='A Python client for the Nitrous-Oxide API',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='NITROUSOXIDE',  
+    author_email='hello@nitrous-oxi.de',  
+    url='https://github.com/NitrousSL/nitrous-py',  
     packages=find_packages(),
     install_requires=[
         'requests',
@@ -12,7 +17,13 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'nitrous-oxi=nitrous_oxi.cli:main',
+            'nitrous-oxide=nitrous_oxide.cli:main',
         ],
     },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
