7f25ba35afcc4b345bdddb05bbd255a0f6030641
httpsisbetter <119723641+httpsisbetter@users.noreply.github.com>
2024-06-26 19:27:10 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='nitrous-oxi',
-    version='0.1.0',
+    version='0.2.0',
     description='A Python client for the Nitrous-Oxi API',
     packages=find_packages(),
     install_requires=[
         'requests',
         'prompt_toolkit',
         'rich'
+        'argparse'
     ],
     entry_points={
         'console_scripts': [
